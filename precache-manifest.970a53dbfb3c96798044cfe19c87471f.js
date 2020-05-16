7579fab1153b47cc4f8aaf5c384040fc0853089a
Sayee97 <sayee0612@gmail.com>
2020-05-16 16:45:50 -0700

    Updates

---
+++ precache-manifest.970a53dbfb3c96798044cfe19c87471f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e278f8f900b982aab50d3907abfd4fc",
+    "url": "/sayee-website/index.html"
+  },
+  {
+    "revision": "ce3f52f5a96598dee50d",
+    "url": "/sayee-website/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "d0e29b39fa074fb0884b",
+    "url": "/sayee-website/static/js/2.36b75289.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sayee-website/static/js/2.36b75289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce3f52f5a96598dee50d",
+    "url": "/sayee-website/static/js/main.faca4c40.chunk.js"
+  },
+  {
+    "revision": "4ba26b10558376f6a6d2",
+    "url": "/sayee-website/static/js/runtime-main.a2a6188f.js"
+  }
+]);
